2a2e88b9069a1f2b13cedce2721de1ed0204bbee
upitroma <allen@rudolphweb.com>
2020-09-25 18:33:37 -0400

    finished for now, might polish later

--- sidequest2.py
+++ sidequest2.py
@@ -1,4 +1,38 @@
 #!/bin/python3.8
-print("Welcome to sidequest 2!\nThis flag has been fragmented into 3 pieces.\nNTI{[piece1]-[piece2]-[piece3]}\nExample: NTI{1234-5678-4321}")
-s=input()
-print(s)
+
+
+print("\n\n\nWelcome to sidequest 2/3!\nThis flag has been fragmented into 3 pieces.\nThe flag is in the format: NTI{1234-1234-1234}\n")
+
+print("The first fragment is a freebie: 9341\n")
+f1=input("ENTER FRAGMENT [1]>")
+
+#fragment 1
+if(f1!="9341"):
+    print("INVALID FRAGMENT. ACCESS DENIED.")
+    exit()
+
+
+print("\n\n\nACCESS GRANTED.\n")
+print("The next fragment will require you have read permissions for this executable.\nCome back with a more privileged user and look for the fragment.\n")
+f2=input("ENTER FRAGMENT [2]>")
+
+#fragment 2
+if(f2!="8392"):
+    print("INVALID FRAGMENT. ACCESS DENIED.")
+    exit()
+    
+
+print("\n\n\nACCESS GRANTED.\n")
+print("You're almost there. Decode the hash to find the last fragment.\n*hint: find an online md5 hash cracker*")
+f3=input("ENTER FRAGMENT [3]>")
+
+
+#fragment 3
+import hashlib
+if(hashlib.md5(f3.encode()).hexdigest()!="12f73080e04ce0d8e95defb577ebc3f4"):
+    print("INVALID FRAGMENT. ACCESS DENIED.")
+    exit()
+
+
+print("\n\n\nACCESS GRANTED.\n")
+print("NTI{"+f1+"-"+f2+"-"+f3+"}\n")
